481ad97c5fb29fd72b74d22443eee1fc619a3a0d
elaineads <elaineaps@outlook.com.br>
2022-12-21 20:12:44 -0300

    Add animação ao botão de menu lateral

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,8 @@
-const btnMenu = document.querySelector('.btn_menu')
-const sidebar = document.querySelector('.sidebar')
+const btnMenu = document.querySelector('.btn_menu');
+const sidebar = document.querySelector('.sidebar');
 
 /* adicionando evento ao botão */
 btnMenu.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar_ativa') /* toggle alterna a visualiação */ 
-})
+    btnMenu.classList.toggle('active');
+    sidebar.classList.toggle('active'); /* toggle alterna a visualiação */ 
+});
