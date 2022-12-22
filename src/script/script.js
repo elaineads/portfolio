aa74667f7d3487144b985d928e12cfbaa25aac0b
elaineads <elaineaps@outlook.com.br>
2022-12-21 23:43:55 -0300

    Trocando imagem de perfil e resolvendo bugs

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,10 @@
 const btnMenu = document.querySelector('.btn_menu');
 const sidebar = document.querySelector('.sidebar');
+const pagina = document.querySelector('.container')
 
 /* adicionando evento ao botão */
 btnMenu.addEventListener('click', () => {
-    btnMenu.classList.toggle('active');
-    sidebar.classList.toggle('active'); /* toggle alterna a visualiação */ 
+    btnMenu.classList.toggle('active'); /* toggle alterna a visualiação */ 
+    sidebar.classList.toggle('active');
+    pagina.classList.toggle('active'); 
 });
